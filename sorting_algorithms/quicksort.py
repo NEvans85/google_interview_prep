ee279b04b4ee0cc5357e6f5d4d1f33d4d273b60c
Nicholas Evans <nevans85.pro@gmail.com>
2017-12-11 10:20:50 -0800

    revise quicksort description

--- sorting_algorithms/quicksort.py
+++ sorting_algorithms/quicksort.py
@@ -1,13 +1,14 @@
 """
-Quicksort is a sorting algorithms which executes at an average of O(n*log(n)).
+Quicksort is a sorting algorithm with an average complexity of O(n*log(n)).
 It works by choosing a pivot element, seperating the remaining elements into
 a list of values less then the pivot and a list of elements greater then the 
-element, then performing quicksort on the less than and greater than elements.
-Using this recursive algorithm, each step takes O(n) and the number of steps is
-logarithmically related to the number of elements. The following implementation 
-uses the first element as the pivot. The the worst case scenario, for this
-implementation is a list which is already sorted or in reverse order. This would
-require n steps, resulting in a complexity of O(n**2).
+element, then performing quicksort on the left (less than) and right (greater than) 
+elements. Using this recursive algorithm, each step takes O(n) and the number of 
+steps is logarithmically related to the number of elements. The following 
+implementation uses the first element as the pivot. The worst case scenario for 
+this implementation is a list which is already sorted or in reverse order, causing
+all the elements to end up in either the left or right list for each iteraion. This 
+would require n steps, resulting in a complexity of O(n**2).
 """
 
 def quicksort(iList):
