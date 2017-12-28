17f4c164e0e0db0adddec1185018a5da78f83045
Nicholas Evans <nevans85.pro@gmail.com>
2017-12-28 09:39:40 -0800

    add Trie description

--- trees_and_graphs/trie_tree.py
+++ trees_and_graphs/trie_tree.py
@@ -1,3 +1,23 @@
+"""
+A trie tree (trie from re-trie-val) is commonly used to hold a collection
+of words. The benefit of a trie is that checking for inclusion of a word
+happens in O(L) where L is the length of the word. It is also good for
+finding all words which start with a given prefix for features like
+autocomplete.
+The following implementation uses a Trie class and a Node class.
+THe Node class can fetch it's children by a value (returning False if the
+child is non-existant), add a new child node (returning the new child),
+and fetch all suffixes which branch from it (using a BFS through it's
+descendents).
+The Trie class starts with a blank node (with a value of "_") and can
+add words (or a list of words) using the Node class' methods: getChild
+(to find the last existing node with the word's characters) and addChild
+(to append new nodes for the remaining characters). It can also find all
+words with a given prefix by traversing the Trie through the characters
+in a given string then using the Node's findSuffixes method.
+
+"""
+
 class Node:
     def __init__(self, data):
         self.data = data
