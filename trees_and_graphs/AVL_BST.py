293a8f8597cc1f78c5ba9c21c05ea2a9f1c5e7db
Nicholas Evans <nevans85.pro@gmail.com>
2017-12-31 13:15:04 -0800

    start writing AVL BST - Node, insert, balance and rotates

---
+++ trees_and_graphs/AVL_BST.py
@@ -0,0 +1,72 @@
+"""
+The AVL tree, named for its inventors Adelson-Velsky and Landis, is a self
+balancing binary search tree. Each node is given a balance factor which
+is the difference between the heights of its left and right branches. If
+this factor is ever greater than 1 (or less than -1) for a node the
+branches are rotated around that node. Rotations are made when any
+modification to the tree results in this inbalance.
+"""
+
+class AVLTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value, root = self.root):
+        if root:
+            if root.data < value:
+                if root.right:
+                    self.insert(value, root.right)
+                else:
+                    root.right = self.Node(value, root)
+            else:
+                if root.left:
+                    self.insert(value, root.left)
+                else:
+                    root.left = self.Node(value, root)
+            self.balance(root)
+        else:
+            self.root = self.Node(value)
+
+    def balance(self, root):
+        diff = root.heightDiff()
+        if diff > 1:
+            if root.left.heightDiff() == -1:
+                self.rotateLeft(root.left)
+            self.rotateRight(root)
+        elif diff < -1:
+            if root.right.heightDiff() == 1:
+                self.rotateRight(root.right)
+            self.rotateLeft(root)
+
+    def rotateLeft(self, root):
+        root.right.parent = root.parent
+        root.parent = root.right
+        root.right = root.right.left
+
+    def rotateRight(self, root):
+        root.left.parent = root.parent
+        root.parent = root.left
+        root.left = root.left.right
+
+    class Node:
+        def __init__(self, data, parent = None):
+            self.data = data
+            self.parent = parent
+            self.left = None
+            self.right = None
+
+        def height(self):
+            return max(self.childHeights())
+
+        def childHeights(self):
+            leftH = 0
+            rightH = 0
+            if self.left:
+                leftH = 1 + self.left.height()
+            if self.right:
+                rightH = 1 + self.right.height()
+            return [leftH, rightH]
+
+        def heightDiff(self):
+            leftH, rightH = self.childHeights()
+            return leftH - rightH
--- trees_and_graphs/binary_tree.py
+++ trees_and_graphs/binary_tree.py
@@ -1,6 +1,9 @@
 """
-A Binary tree is on in which nodes have, at most, two children. These
+A Binary tree is a tree in which nodes have, at most, two children. These
 children are often stored in a left and right attribute.
+The Binary Search Tree (BST) is a binary tree in which each node's
+left child's data is less that the node's value and the right child's data
+is greater.
 """
 
 class Node:
@@ -8,3 +11,41 @@ class Node:
         self.data = data
         self.right = None
         self.left = None
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def bfs(self, target, root = self.root):
+        if root:
+            queue = [root]
+            while len(queue) > 0:
+                currNode = queue.pop(0)
+                if currNode.data == target:
+                    return currNode
+                if currNode.left:
+                    queue.append(currNode.left)
+                if currNode.right:
+                    queue.append(currNode.right)
+        return False
+
+    def dfs(self, target, root = self.root):
+        if root:
+            if root.data == target:
+                return root
+            rightResult = self.dfs(target, root.right):
+            if rightResult:
+                return rightResult
+            leftResult = self.dfs(target, root.left):
+            if leftResult:
+                return leftResult
+        return False
+
+    def inOrderTraversal(self, root = self.root):
+        results = []
+        if root.left:
+            results += inOrderTraversal(root.left)
+        results.append(root.data)
+        if root.right:
+            results += inOrderTraversal(root.right)
+        return results
