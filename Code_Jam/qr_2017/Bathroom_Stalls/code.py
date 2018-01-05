b8c15a9720d846c03cb3278c8ec81cf253f4d147
Nicholas Evans <nevans85.pro@gmail.com>
2018-01-05 12:21:43 -0800

    add bathroom stalls prompt

---
+++ Code_Jam/qr_2017/Bathroom_Stalls/code.py
@@ -0,0 +1,8 @@
+f = open("A-small-practice-1.in.txt", 'r')
+o = open("A-small-practice-1.out.txt", 'w')
+# f = open("A-small-practice-2.in.txt", 'r')
+# o = open("A-small-practice-2.out.txt", 'w')
+# f = open("A-large-practice.in.txt", 'r')
+# o = open("A-large-practice.old.txt", 'w')
+n = int(f.readline())
+
